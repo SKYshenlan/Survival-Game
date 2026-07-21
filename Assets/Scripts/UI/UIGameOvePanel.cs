e918311c0f1c5052304a9f9e2e654a724242fef3
SKYshenlan <2844614009@qq.com>
2026-07-21 19:07:09 +0800

    添加重新开始系统

--- Assets/Scripts/UI/UIGameOvePanel.cs
+++ Assets/Scripts/UI/UIGameOvePanel.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using QFramework;
+using UnityEngine.SceneManagement;
 
 namespace GameUI
 {
@@ -12,8 +13,19 @@ namespace GameUI
 		protected override void OnInit(IUIData uiData = null)
 		{
 			mData = uiData as UIGameOvePanelData ?? new UIGameOvePanelData();
-			// please add init code here
-		}
+            // please add init code here
+            //静态管理器 每帧执行管理器的全局队列
+            ActionKit.OnUpdate.Register(() =>
+			{
+				//监听空格
+				if (Input.GetKeyDown(KeyCode.Space))
+				{
+					this.CloseSelf();
+					//切换场景
+					SceneManager.LoadScene("SampleScene");
+				}
+			}).UnRegisterWhenGameObjectDestroyed(gameObject);//gameObject被销毁或隐藏时注销事件
+        }
 		
 		protected override void OnOpen(IUIData uiData = null)
 		{
