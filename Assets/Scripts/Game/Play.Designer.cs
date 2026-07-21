7a5b1972ffce9c7286119f9d97495892c2635ab6
SKYshenlan <2844614009@qq.com>
2026-07-21 15:11:28 +0800

    创建项目

---
+++ Assets/Scripts/Game/Play.Designer.cs
@@ -0,0 +1,11 @@
+// Generate Id:858c36aa-2f56-4dca-bd31-61a584bf4667
+using UnityEngine;
+
+namespace Brotato
+{
+	public partial class Play
+	{
+		public SpriteRenderer Sprite;
+		
+	}
+}
---
+++ Assets/Scripts/Game/Play.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using QFramework;
+
+namespace Brotato
+{
+	public partial class Play : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+			Debug.Log("a");
+		}
+	}
+}
