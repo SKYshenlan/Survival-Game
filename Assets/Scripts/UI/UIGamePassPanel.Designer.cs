d1b5e0f662db759c7f35c93f04b2bf51b33a54cc
SKYshenlan <2844614009@qq.com>
2026-07-22 21:59:37 +0800

    添加胜利UI提示

---
+++ Assets/Scripts/UI/UIGamePassPanel.Designer.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using QFramework;
+
+namespace GameUI
+{
+	// Generate Id:78e38e00-1d44-42c2-845d-5844edf3580f
+	public partial class UIGamePassPanel
+	{
+		public const string Name = "UIGamePassPanel";
+		
+		
+		private UIGamePassPanelData mPrivateData = null;
+		
+		protected override void ClearUIComponents()
+		{
+			
+			mData = null;
+		}
+		
+		public UIGamePassPanelData Data
+		{
+			get
+			{
+				return mData;
+			}
+		}
+		
+		UIGamePassPanelData mData
+		{
+			get
+			{
+				return mPrivateData ?? (mPrivateData = new UIGamePassPanelData());
+			}
+			set
+			{
+				mUIData = value;
+				mPrivateData = value;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIGamePassPanel.cs
@@ -0,0 +1,46 @@
+﻿using QFramework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GameUI
+{
+	public class UIGamePassPanelData : UIPanelData
+	{
+	}
+	public partial class UIGamePassPanel : UIPanel
+	{
+		protected override void OnInit(IUIData uiData = null)
+		{
+			mData = uiData as UIGamePassPanelData ?? new UIGamePassPanelData();
+            // please add init code here
+            //静态管理器 每帧执行管理器的全局队列
+            ActionKit.OnUpdate.Register(() =>
+            {
+                //监听空格
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    this.CloseSelf();
+                    //切换场景
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);//gameObject被销毁或隐藏时注销事件
+        }
+		
+		protected override void OnOpen(IUIData uiData = null)
+		{
+		}
+		
+		protected override void OnShow()
+		{
+		}
+		
+		protected override void OnHide()
+		{
+		}
+		
+		protected override void OnClose()
+		{
+		}
+	}
+}
