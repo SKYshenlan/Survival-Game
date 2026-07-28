a32e168fd22a4220a40e128da7c04ab0f29c4a86
SKYshenlan <2844614009@qq.com>
2026-07-28 19:58:29 +0800

    修改初始数据

--- Assets/Scripts/Game/Global.cs
+++ Assets/Scripts/Game/Global.cs
@@ -10,10 +10,10 @@ namespace SurvivalGame
         /// <summary>
         /// 经验 主动通知UI发生改变的响应式数据容器
         /// </summary>
-        public static BindableProperty<int> Exp = new BindableProperty<int>();
+        public static BindableProperty<int> Exp = new BindableProperty<int>(0);
         /// <summary>
         /// 等级
         /// </summary>
-        public static BindableProperty<int> Leve = new BindableProperty<int>();
+        public static BindableProperty<int> Leve = new BindableProperty<int>(1);
     }
 }
