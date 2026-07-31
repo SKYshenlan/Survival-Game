f0135b65d9f15d588541f646678bf5614d500341
SKYshenlan <2844614009@qq.com>
2026-07-31 16:15:44 +0800

    修复金币储存

--- Assets/Scripts/UI/UIGamePanel.cs
+++ Assets/Scripts/UI/UIGamePanel.cs
@@ -57,7 +57,7 @@ namespace GameUI
                 leve.text = $"等级「{Leve}」";
             }).UnRegisterWhenGameObjectDestroyed(gameObject);//gameObject被销毁或隐藏时注销事件
             //从本地磁盘加载上次保存的金币数量
-            Global.Coins.Value = PlayerPrefs.GetInt("coin", 0);
+            Global.Coins.Value = PlayerPrefs.GetInt("_coins", 0);
             Global.Coins.RegisterWithInitValue(_coins =>
             {
                 PlayerPrefs.SetInt(nameof(_coins), _coins);
