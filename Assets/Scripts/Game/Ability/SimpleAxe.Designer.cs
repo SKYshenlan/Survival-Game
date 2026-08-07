732a07a8e82d17a3f738b734275e2b4e3d3da564
SKYshenlan <2844614009@qq.com>
2026-08-07 17:01:33 +0800

    飞斧攻击逻辑

---
+++ Assets/Scripts/Game/Ability/SimpleAxe.Designer.cs
@@ -0,0 +1,11 @@
+// Generate Id:a6dfb5ec-2f04-4c87-aa8f-3916b2378be2
+using UnityEngine;
+
+namespace Brotato
+{
+	public partial class SimpleAxe
+	{
+		public BoxCollider2D Axe;
+		
+	}
+}
---
+++ Assets/Scripts/Game/Ability/SimpleAxe.cs
@@ -0,0 +1,58 @@
+﻿using QFramework;
+using SurvivalGame;
+using UnityEngine;
+
+namespace Brotato
+{
+	public partial class SimpleAxe : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+		}
+		private float Second = 0;
+        private void Update()
+        {
+			Second += Time.deltaTime;
+			if( Second >= 1f)
+			{
+				Axe.Instantiate()
+					.Show()
+                    .Position(this.Position())
+                    .Self(self =>
+                    {
+                        //获取刚体
+                        var rigidbody2D = self.GetComponent<Rigidbody2D>();
+                        //随机x位置
+                        var ranX = RandomUtility.Choose(- 8, -5,-3 ,3,5, 8);
+                        //随机y位置
+                        var ranY = RandomUtility.Choose(3,5, 8);
+                        //设置一个瞬间的绝对速度
+                        rigidbody2D.velocity = new Vector2(ranX, ranY);
+						self.OnTriggerEnter2DEvent(call =>
+						{
+                            var Box = call.GetComponent<HurtBox>();
+                            if (Box)
+                            {
+                                if (Box.Owner.CompareTag("Enemy"))
+                                {
+                                    Box.Owner.GetComponent<Enemy>().Hide(Global.Atk.Value);
+                                }
+                            }
+                        }).UnRegisterWhenGameObjectDestroyed(self);
+                        ActionKit.OnUpdate.Register(() =>
+                        {
+                            if (Play.Defaulf)
+                            {
+                                if (Play.Defaulf.Position().y - self.Position().y > 15)
+                                {
+                                    self.DestroyGameObjGracefully();
+                                }
+                            }
+                        }).UnRegisterWhenGameObjectDestroyed(self);
+                    });
+				Second = 0f;
+			}
+        }
+    }
+}
