ac44a0949608afbb8229fda9c7648cff1df4fb80
SKYshenlan <2844614009@qq.com>
2026-08-07 17:29:36 +0800

    添加非标攻击

---
+++ Assets/Scripts/Game/Ability/SimpleKnife.Designer.cs
@@ -0,0 +1,11 @@
+// Generate Id:74d72a24-2835-4e72-b74a-ad45ea90e0ce
+using UnityEngine;
+
+namespace Brotato
+{
+	public partial class SimpleKnife
+	{
+		public SpriteRenderer Knife;
+		
+	}
+}
---
+++ Assets/Scripts/Game/Ability/SimpleKnife.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using QFramework;
+using SurvivalGame;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+namespace Brotato
+{
+    public partial class SimpleKnife : ViewController
+    {
+        private float Second = 0;
+        void Start()
+        {
+            // Code Here
+        }
+        private void Update()
+        {
+            Second += Time.deltaTime;
+            if (Second >= 1)
+            {
+                Second = 0;
+                //查找满足条件的对象并返回数组         只查找激活状态             不进行排序
+                var enemy = FindObjectsByType<Enemy>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+                var en = enemy.OrderBy(enemy => (Play.Defaulf.transform.position - enemy.transform.position).magnitude).FirstOrDefault();
+                if (en)
+                {
+                    Knife.Instantiate()
+                        .Position(this.Position())
+                        .Show()
+                        .Self(self =>
+                        {
+                            //获取刚体
+                            var rigidbody2D = self.GetComponent<Rigidbody2D>();
+                            var dir = (en.Position() - Play.Defaulf.Position()).normalized;
+                            //设置一个瞬间的绝对速度
+                            rigidbody2D.velocity = dir * 10;
+                            self.OnTriggerEnter2DEvent(call =>
+                            {
+                                var Box = call.GetComponent<HurtBox>();
+                                if (Box)
+                                {
+                                    if (Box.Owner.CompareTag("Enemy"))
+                                    {
+                                        Box.Owner.GetComponent<Enemy>().Hide(Global.Atk.Value);
+                                        self.DestroyGameObjGracefully();
+                                    }
+                                }
+                            }).UnRegisterWhenGameObjectDestroyed(self);
+                            ActionKit.OnUpdate.Register(() =>
+                            {
+                                if (Play.Defaulf)
+                                {
+                                    if ((Play.Defaulf.Position() - self.Position()).magnitude > 20)
+                                    {
+                                        self.DestroyGameObjGracefully();
+                                    }
+                                }
+                            }).UnRegisterWhenGameObjectDestroyed(self);
+                        });
+                    en.Hide(Global.Atk.Value);
+                }
+            }
+        }
+    }
+}
