0d4a027d7df425eda856e114a0c9546d96a99694
SKYshenlan <2844614009@qq.com>
2026-08-11 17:25:09 +0800

    调整

--- Assets/Scripts/Game/Ability/Simple_Sword.Designer.cs
+++ Assets/Scripts/Game/Ability/Simple_Sword.Designer.cs
@@ -1,9 +1,11 @@
-﻿// Generate Id:6ff0cdd0-c174-4848-9d8d-6d93a52ddcb6
+// Generate Id:dd1bf9b3-b484-4c45-b139-460169f25c26
 using UnityEngine;
 
 namespace Brotato
 {
 	public partial class Simple_Sword
-    {
+	{
+		public BoxCollider2D Sword;
+		
 	}
 }
--- Assets/Scripts/Game/Ability/Simple_Sword.cs
+++ Assets/Scripts/Game/Ability/Simple_Sword.cs
@@ -14,22 +14,91 @@ namespace Brotato
 		}
         private void Update()
         {
-			//Second += Time.deltaTime;
-			//if( Second >= Global.AtkSpeed.Value)
-			//{
-			//	Second = 0;
-   //             //查找满足条件的对象并返回数组         只查找激活状态             不进行排序
-   //             var enemy = FindObjectsByType<Enemy>(FindObjectsInactive.Exclude,FindObjectsSortMode.None);
-   //             foreach (var item in enemy)
-   //             {
-   //                 //计算玩家和敌人的距离
-   //                 var dir = (Play.Defaulf.transform.position - item.transform.position).magnitude;
-   //                 if (dir <= 5)
-   //                 {
-   //                     item.Hide(Global.Atk.Value);
-   //                 }
-   //             }
-   //         }
+            Second += Time.deltaTime;
+            if (Second >= Global.AtkSpeed.Value)
+            {
+                Second = 0;
+                //查找满足条件的对象并返回数组         只查找激活状态             不进行排序
+                var enemy = FindObjectsByType<Enemy>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+                foreach (var item in enemy)
+                {
+                    //计算玩家和敌人的距离
+                    var dir = (Play.Defaulf.transform.position - item.transform.position).magnitude;
+                    if (dir <= 5)
+                    {
+                        //item.Hide(Global.Atk.Value);
+                        Sword.Instantiate()
+                            .Position(item.Position() + Vector3.left*0.25f)
+                            .Show()
+                            .Self(self =>
+                            {
+                                var selfCache = self;
+                                selfCache.OnTriggerEnter2DEvent(call =>
+                                {
+                                    var hurtBox = call.GetComponent<HurtBox>();
+                                    if (hurtBox != null)
+                                    {
+                                        if (hurtBox.Owner.CompareTag("Enemy"))
+                                        {
+                                            hurtBox.Owner.GetComponent<Enemy>().Hide(Global.Atk.Value);
+                                        }
+                                    }
+                                }).UnRegisterWhenGameObjectDestroyed(gameObject);
+                                //劈砍动画,创建一个新的动作序列
+                                ActionKit.Sequence()
+                                //添加一个回调动作
+                                .Callback(() =>
+                                {
+                                    // 禁用selfCache
+                                    selfCache.enabled = false;
+                                })
+                                //添加一个并行动作组
+                                .Parallel(p =>
+                                {
+                                    //在并行组中添加一个插值动作，从0到10（值），持续时间0.2秒，z为插值因子（0~1）
+                                    p.Lerp(0, 10, 0.2f, (z) =>
+                                    {
+                                        // 将自身localEulerAngles的Z设置为缓存的Z值乘以z，实现Z轴旋转
+                                        self.transform.localEulerAngles = selfCache.transform.localEulerAngles.Z(z);
+                                    });
+                                    //在并行组中添加一个顺序子序列
+                                    p.Append(ActionKit.Sequence()
+                                        //子序列第一步：缩放从0到1.25，0.1秒
+                                        .Lerp(0, 1.25f, 0.1f, scale =>{selfCache.LocalScale(scale);})
+                                        //子序列第二步：缩放从1.25回到1，0.1秒
+                                        .Lerp(1.25f, 1, 0.1f, scale =>{selfCache.LocalScale(scale);})
+                                    );
+                                })
+                                .Callback(() =>{selfCache.enabled = true;})
+                                .Parallel(p =>
+                                {
+                                    p.Lerp(10, -180, 0.2f, z =>
+                                    {
+                                        selfCache.transform.localEulerAngles = selfCache.transform.localEulerAngles.Z(z);
+                                        //在并行组中添加一个顺序子序列
+                                        p.Append(ActionKit.Sequence()
+                                            //子序列第一步：缩放从0到1.25，0.1秒
+                                            .Lerp(1, 1.25f, 0.1f, scale => { selfCache.LocalScale(scale); })
+                                            //子序列第二步：缩放从1.25回到1，0.1秒
+                                            .Lerp(1.25f, 1, 0.1f, scale => { selfCache.LocalScale(scale); })
+                                        );
+                                    });
+                                })
+                                .Callback(() => { selfCache.enabled = true; })
+                                .Lerp(-180, 0, 0.3f, z =>
+                                {
+                                    selfCache.transform.localEulerAngles = selfCache.transform.localEulerAngles.Z(z);
+                                    selfCache.LocalScale(z.Abs() / 180);
+                                })
+                                .Start(this, () =>
+                                {
+                                    //动作执行完删除对象
+                                    selfCache.DestroyGameObjGracefully();
+                                });
+                            });
+                    }
+                }
+            }
         }
     }
 }
